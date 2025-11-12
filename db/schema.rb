e433b99564b61efca0c61b19f2099e9e67d20d65
Revelo Devs <devs@revelo.com>
2025-11-12 15:15:52 -0300

    fix: remove pg_vector

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.1].define(version: 2025_11_12_172258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-  enable_extension "vector"
 
   create_table "github_repos", force: :cascade do |t|
     t.datetime "created_at", null: false
